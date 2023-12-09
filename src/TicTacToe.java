d2581938c84c126dbcb517c96ffbabd22b6e153c
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-09 10:24:49 +0500

    Some changes to improve the readability of the code

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-
     static void output(char[][] ttt, int row) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -171,6 +170,7 @@ public class TicTacToe {
         final int r = 3, c = 3;
         int input1 = 0, input2 = 0;
         char[][] ttt = new char[r][c];
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 ttt[i][j] = '*';
