6e0fd31e22d3afb03c50c95473a147337420f5b1
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-09 10:33:21 +0500

    Test for player-1 turn added

--- src/test.java
+++ src/test.java
@@ -3,4 +3,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeTest {
 
+    @Test
+    void testPlayer1() {
+        char[][] ttt = new char[][]{{'*', '*', '*'}, {'*', '*', '*'}, {'*', '*', '*'}};
+        TicTacToe.player1(ttt, 3, 1);
+        assertEquals('X', ttt[0][0]);
+    }
+
 }
