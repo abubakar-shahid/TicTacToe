c2f6f950ac4a9aa3c8d2b487e90a379e5b3f2c1c
abubakarshahid1309 <abubakarshahid1309@gmail.com>
2023-12-09 10:37:56 +0500

    testing player-1 win

--- src/test.java
+++ src/test.java
@@ -31,4 +31,11 @@ class TicTacToeTest {
         assertEquals(0, result);
     }
 
+    @Test
+    void testEnding1CheckPlayer1Wins() {
+        char[][] ttt = new char[][]{{'X', 'O', 'X'}, {'O', 'X', '*'}, {'*', '*', 'X'}};
+        int result = TicTacToe.ending1Check(ttt, 3);
+        assertEquals(0, result);
+    }
+
 }
